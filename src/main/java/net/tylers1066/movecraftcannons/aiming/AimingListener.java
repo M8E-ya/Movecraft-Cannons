d31da70a593401ec7eadf98c5ad903c3894b4296
galacticwarrior9 <zareef1@yahoo.com>
2021-11-10 23:34:17 +0000

    Add ability to fire already-loaded cannons on a ship by left-clicking with a clock

--- src/main/java/net/tylers1066/movecraftcannons/aiming/AimingListener.java
+++ src/main/java/net/tylers1066/movecraftcannons/aiming/AimingListener.java
@@ -1,7 +1,13 @@
 package net.tylers1066.movecraftcannons.aiming;
 
+import at.pavlov.cannons.Cannons;
+import at.pavlov.cannons.Enum.InteractAction;
+import at.pavlov.cannons.cannon.Cannon;
 import net.countercraft.movecraft.craft.Craft;
 import net.countercraft.movecraft.craft.CraftManager;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
+import net.tylers1066.movecraftcannons.listener.DetectionListener;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -15,10 +21,6 @@ public class AimingListener implements Listener {
 
     @EventHandler(priority = EventPriority.MONITOR)
     public void onInteract(PlayerInteractEvent event) {
-        if (event.getAction() != Action.RIGHT_CLICK_AIR && event.getAction() != Action.RIGHT_CLICK_BLOCK) {
-            return;
-        }
-
         if (event.getItem() == null || event.getItem().getType() != Material.CLOCK) {
             return;
         }
@@ -28,6 +30,16 @@ public class AimingListener implements Listener {
         if (craft == null) {
             return;
         }
-        AimingUtils.aimCannonsOnCraft(craft, player);
+
+        if (event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK) {
+            for (Cannon cannon: DetectionListener.cannonsOnCraft.get(craft)) {
+                Cannons.getPlugin().getFireCannon().playerFiring(cannon, player, InteractAction.fireAutoaim);
+            }
+            player.sendMessage(Component.text("Attempting to fire already-loaded cannons!", TextColor.color(0xc3f09e)));
+        }
+
+        else if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            AimingUtils.aimCannonsOnCraft(craft, player);
+        }
     }
 }
