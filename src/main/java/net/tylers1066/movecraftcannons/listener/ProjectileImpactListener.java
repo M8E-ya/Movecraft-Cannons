1d4141021e2b682fa45be759ac82abb7ac36a9a8
galacticwarrior9 <zareef1@yahoo.com>
2021-12-15 00:37:50 +0000

    Stop ships from blowing themselves up (take 2)

--- src/main/java/net/tylers1066/movecraftcannons/listener/ProjectileImpactListener.java
+++ src/main/java/net/tylers1066/movecraftcannons/listener/ProjectileImpactListener.java
@@ -4,11 +4,14 @@ import at.pavlov.cannons.event.ProjectileImpactEvent;
 import net.countercraft.movecraft.combat.tracking.DamageManager;
 import net.countercraft.movecraft.craft.Craft;
 import net.countercraft.movecraft.craft.CraftManager;
+import net.countercraft.movecraft.craft.PilotedCraft;
 import net.countercraft.movecraft.craft.PlayerCraft;
 import net.countercraft.movecraft.util.MathUtils;
 import net.tylers1066.movecraftcannons.MovecraftCannons;
 import net.tylers1066.movecraftcannons.config.Config;
 import net.tylers1066.movecraftcannons.damagetype.ProjectileImpactDamage;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -36,5 +39,23 @@ public class ProjectileImpactListener implements Listener {
 
         PlayerCraft playerCraft = (PlayerCraft) craft;
         DamageManager.getInstance().addDamageRecord(playerCraft, cause, new ProjectileImpactDamage());
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void antiFriendlyFireListener(ProjectileImpactEvent event) {
+        Location impactLocation = event.getImpactLocation();
+        Player cannoneer = Bukkit.getPlayer(event.getShooterUID());
+        if (cannoneer == null) {
+            return;
+        }
+
+        Craft craft = CraftManager.getInstance().getCraftByPlayer(cannoneer);
+        if (craft == null) {
+            return;
+        }
+
+        if (MathUtils.locationNearHitBox(craft.getHitBox(), impactLocation, 1.5)) {
+            event.setCancelled(true);
+        }
     }
 }
